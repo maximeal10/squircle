ca35ae6ffc7a51652c42b074470dbdd03ca62a6a
Maxime Ashurov <maxim.ashurov@octopod.com>
2022-01-19 09:50:07 +0300

    multisize, url params

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var ctx = document.createElement('canvas').getContext('2d');
-ctx.canvas.width = 500;
-ctx.canvas.height = 500;
-ctx.canvas.style.width = '500px';
-ctx.canvas.style.height = '500px';
-ctx.width = 500;
-ctx.height = 500;
+var sizes = [128, 96, 64, 40, 32, 24, 20];
+var ctxs = [];
 
-var ctx2 = document.createElement('canvas').getContext('2d');
-ctx2.canvas.width = 500;
-ctx2.canvas.height = 500;
-ctx2.canvas.style.width = '500px';
-ctx2.canvas.style.height = '500px';
-ctx2.width = 500;
-ctx2.height = 500;
+for (const size of sizes) {
 
-document.body.appendChild(ctx.canvas);
-document.body.appendChild(ctx2.canvas);
+    let height = size + 20;
 
-var rectWidthInput = document.getElementById("rectWidthInput");
-var rectWidthLabel = document.getElementById("rectWidthLabel");
+    var label = document.createElement('div')
+    label.style.paddingLeft = '10px';
+    label.style.paddingTop = '10px';
+    label.innerText = size+'x'+size
 
-var rectWidthInput = document.getElementById("rectWidthInput");
-var rectWidthLabel = document.getElementById("rectWidthLabel");
+    var ctx = document.createElement('canvas').getContext('2d');
+    ctx.canvas.width = 500;
+    ctx.canvas.height = height;
+    ctx.canvas.style.width = '500px';
+    ctx.canvas.style.height = height+'px';
+    ctx.width = 500;
+    ctx.height = height;
+
+    ctxs.push(ctx);
+    document.body.appendChild(label);
+    document.body.appendChild(ctx.canvas);
+}
+
 
 var sizeInput = document.getElementById("sizeInput");
 var sizeLabel = document.getElementById("sizeLabel");
@@ -29,24 +30,21 @@ var sizeLabel = document.getElementById("sizeLabel");
 var cornerInput = document.getElementById("cornerInput");
 var cornerLabel = document.getElementById("cornerLabel");
 
-function updateRectWidth() {
-    rectWidthLabel.innerHTML = "Rect width: " + rectWidthInput.value;
-    // sizeInput.max = Math.min(rectWidthInput.value, rectWidthInput.value) / 2;
-    // updateSize();
-    draw();
-}
+let url = new URL(window.location.href);
+
+sizeInput.value = url.searchParams.get("corner") || 0.25;
+cornerInput.value = url.searchParams.get("smoothing") || 0.5;
 
 function updateSize() {
     sizeLabel.innerHTML = "Corner size: " + sizeInput.value;
-    draw();
+    drawAll();
 }
 
 function updateCorner() {
     cornerLabel.innerHTML = "Smoothing: " + cornerInput.value;
-    draw();
+    drawAll();
 }
 
-rectWidthInput.addEventListener('input', updateRectWidth);
 sizeInput.addEventListener('input', updateSize);
 cornerInput.addEventListener('input', updateCorner);
 
@@ -82,12 +80,18 @@ function drawCornerFrame(corner) {
     ctx2.stroke()
 }
 
-function draw() {
+function drawAll() {
+    
+    for (const i in sizes) {
+        const rectWidth = sizes[i]
+        const cornerSize = parseFloat(sizeInput.value) * rectWidth;
+        var corner = parseFloat(cornerInput.value);
 
-    var rectWidth = parseFloat(rectWidthInput.value);
+        draw(ctxs[i], rectWidth, cornerSize, corner)
+    }
+}
 
-    var size = parseFloat(sizeInput.value) * rectWidth;
-    var corner = parseFloat(cornerInput.value);
+function draw(ctx, rectWidth, size, corner) {
 
     var topLeft = [{
         x: offset + 0,
@@ -177,42 +181,41 @@ function draw() {
 
     // frame
 
-    ctx2.strokeStyle = "#000000";
+    // ctx2.strokeStyle = "#000000";
 
-    ctx2.clearRect(0, 0, ctx2.width, ctx2.height);
-    ctx2.beginPath();
+    // ctx2.clearRect(0, 0, ctx2.width, ctx2.height);
+    // ctx2.beginPath();
 
 
-    ctx2.moveTo(topLeft[0].x, topLeft[0].y);
-    ctx2.bezierCurveTo(topLeft[1].x, topLeft[1].y,
-        topLeft[2].x, topLeft[2].y,
-        topLeft[3].x, topLeft[3].y);
+    // ctx2.moveTo(topLeft[0].x, topLeft[0].y);
+    // ctx2.bezierCurveTo(topLeft[1].x, topLeft[1].y,
+    //     topLeft[2].x, topLeft[2].y,
+    //     topLeft[3].x, topLeft[3].y);
 
-    ctx2.lineTo(topRight[0].x, topRight[0].y);
-    ctx2.bezierCurveTo(topRight[1].x, topRight[1].y,
-        topRight[2].x, topRight[2].y,
-        topRight[3].x, topRight[3].y);
+    // ctx2.lineTo(topRight[0].x, topRight[0].y);
+    // ctx2.bezierCurveTo(topRight[1].x, topRight[1].y,
+    //     topRight[2].x, topRight[2].y,
+    //     topRight[3].x, topRight[3].y);
 
-    ctx2.lineTo(bottomRight[0].x, bottomRight[0].y);
-    ctx2.bezierCurveTo(bottomRight[1].x, bottomRight[1].y,
-        bottomRight[2].x, bottomRight[2].y,
-        bottomRight[3].x, bottomRight[3].y);
+    // ctx2.lineTo(bottomRight[0].x, bottomRight[0].y);
+    // ctx2.bezierCurveTo(bottomRight[1].x, bottomRight[1].y,
+    //     bottomRight[2].x, bottomRight[2].y,
+    //     bottomRight[3].x, bottomRight[3].y);
 
-    ctx2.lineTo(bottomLeft[0].x, bottomLeft[0].y);
-    ctx2.bezierCurveTo(bottomLeft[1].x, bottomLeft[1].y,
-        bottomLeft[2].x, bottomLeft[2].y,
-        bottomLeft[3].x, bottomLeft[3].y);
+    // ctx2.lineTo(bottomLeft[0].x, bottomLeft[0].y);
+    // ctx2.bezierCurveTo(bottomLeft[1].x, bottomLeft[1].y,
+    //     bottomLeft[2].x, bottomLeft[2].y,
+    //     bottomLeft[3].x, bottomLeft[3].y);
 
-    ctx2.closePath();
-    ctx2.stroke()
+    // ctx2.closePath();
+    // ctx2.stroke()
 
-    drawCornerFrame(topLeft)
-    drawCornerFrame(topRight)
-    drawCornerFrame(bottomRight)
-    drawCornerFrame(bottomLeft)
+    // drawCornerFrame(topLeft)
+    // drawCornerFrame(topRight)
+    // drawCornerFrame(bottomRight)
+    // drawCornerFrame(bottomLeft)
 
 }
 
-updateRectWidth();
 updateSize();
 updateCorner();
